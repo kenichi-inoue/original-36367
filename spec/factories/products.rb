d40d22b9c2e440e89b69ef86e761ee78b4b16dc4
kenichi-inoue <ken1.inoue13@gmail.com>
2021-12-25 17:53:49 +0900

    手順7：モデルの単体テストコードを書く

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :product do
-    
+    product_name { 'いろいろ' }
+    product_discription { Faker::Lorem.sentence }
+    product_category_id { 2 }
+    product_condition_id { 2 }
+    shipping_burden_id { 2 }
+    shipping_area_id { 2 }
+    days_to_ship_id { 2 }
+    purchase_price { Faker::Number.between(from: 300, to: 9_999_999) }
+
+    after(:build) do |product|
+      product.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+
+    association :user
   end
 end
