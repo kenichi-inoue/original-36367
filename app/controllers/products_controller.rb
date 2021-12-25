5ca8c37ac378cfe3d9ce9dcae85383f401a64614
kenichi-inoue <ken1.inoue13@gmail.com>
2021-12-25 20:15:57 +0900

    check(212125)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  # before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
 
   def index
   end
