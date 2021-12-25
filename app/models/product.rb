81e93ad85c43f4477656a671b146aca30384fc0c
kenichi-inoue <ken1.inoue13@gmail.com>
2021-12-25 10:35:48 +0900

    手順3：ActiveHashを導入

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,11 @@
 class Product < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :product_category
+  belongs_to :product_condition
+  belongs_to :shipping_burden
+  belongs_to :shipping_area
+  belongs_to :days_to_ship
+    
   belongs_to :user
   has_one :purchase
   has_one_attached :image
