ab2e0253299f6e3bc9ed97940f633b5af1e737eb
kenichi-inoue <ken1.inoue13@gmail.com>
2021-12-26 18:30:32 +0900

    手順1−4．バリデーションの処理を書く

--- app/models/purchase_ship.rb
+++ app/models/purchase_ship.rb
@@ -2,4 +2,16 @@ class PurchaseShip
   include ActiveModel::Model
   attr_accessor :user_id, :product_id, :postal_code, :shipping_area_id, :city, :street, :building, :phone, :token
 
+  with_options presence: true do
+    validates :user_id
+    validates :product_id
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :shipping_area_id, numericality: { other_than: 1, message: 'is not included in the list' }
+    validates :city
+    validates :street
+    validates :token
+    validates :phone, length: { in: 10..11 }
+  end
+  validates :phone, format: { with: /\A[0-9]+\z/, message: 'is valid only for half-width number' }
+  
 end
