3dd5f394219c55c3104ee9929ab9a173a60a7f08
kenichi-inoue <ken1.inoue13@gmail.com>
2021-12-26 19:06:27 +0900

    手順1−5．データをテーブルに保存する処理を書く

--- app/models/purchase_ship.rb
+++ app/models/purchase_ship.rb
@@ -13,5 +13,10 @@ class PurchaseShip
     validates :phone, length: { in: 10..11 }
   end
   validates :phone, format: { with: /\A[0-9]+\z/, message: 'is valid only for half-width number' }
-  
+
+  def save
+    purchase = Purchase.create(user_id: user_id, product_id: product_id)
+    Ship.create(postal_code: postal_code, shipping_area_id: shipping_area_id, city: city, street: street, building: building,
+                phone: phone, purchase_id: purchase.id)
+  end
 end
