d92d4744f46f75f7e1d6b4c2f1029c7ac91f774f
kenichi-inoue <ken1.inoue13@gmail.com>
2021-12-29 10:01:36 +0900

    フォームの情報を取得（途中だが）

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,11 +5,12 @@
 
 require("./product_price");
 
-
 require("@rails/ujs").start()
 // require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("../card")
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
