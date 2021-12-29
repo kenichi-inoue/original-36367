1ac847d8b7b66b932fae5755277bc77a77a63740
kenichi-inoue <ken1.inoue13@gmail.com>
2021-12-29 12:24:20 +0900

    2. クレジットカードの情報をフォームから取り除く

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -22,8 +22,14 @@ const pay = () => {
           const renderDom = document.getElementById("charge-form");
           const tokenObj = `<input value=${token} name='token' type="hidden"> `;
           renderDom.insertAdjacentHTML("beforeend", tokenObj);
-          debugger;
+          // debugger;
         }
+        
+      document.getElementById("card-number").removeAttribute("name");
+      document.getElementById("card-cvc").removeAttribute("name");
+      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-exp-year").removeAttribute("name");
+
       });
   
         
