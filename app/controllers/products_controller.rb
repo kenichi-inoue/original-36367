138be4cfc30d597d2312fd381996d509678cc740
kenichi-inoue <ken1.inoue13@gmail.com>
2021-12-29 14:32:10 +0900

    ログイン状態の場合でも、URLを直接入力して自身が出品した商品の商品購入ページに遷移しようとすると、商品の販売状況に関わらずトップページに遷移
    ログイン状態の場合でも、URLを直接入力して自身が出品した商品の商品購入ページに遷移しようとすると、商品の販売状況に関わらずトップページに遷移

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,7 @@ class ProductsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :set_product, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, except: [:index, :show, :new, :create]
+  before_action :move_to_index_three, only: [:edit]
 
   def index
     @products = Product.all
@@ -54,4 +55,9 @@ class ProductsController < ApplicationController
   def move_to_index
     redirect_to action: :index unless current_user.id == @product.user_id
   end
+
+  def move_to_index_three
+    redirect_to root_path if @product.purchase.present? && user_signed_in? && (current_user.id == @product.user_id)
+  end
+
 end
