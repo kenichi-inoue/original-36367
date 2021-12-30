27dee70b0f87ed3830c3e77a7a3c095c78051394
kenichi-inoue <ken1.inoue13@gmail.com>
2021-12-30 21:09:50 +0900

    アクティブハッシュタグ名の変更

--- app/models/days_to_ship.rb
+++ app/models/days_to_ship.rb
@@ -1,9 +1,10 @@
 class DaysToShip < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
-    { id: 2, name: '1~2日で発送' },
-    { id: 3, name: '2~3日で発送' },
-    { id: 4, name: '4~7日で発送' }
+    { id: 2, name: '不要' },
+    { id: 3, name: '要：前泊' },
+    { id: 4, name: '要：後泊' },
+    { id: 5, name: '要：前後泊' }
   ]
 
   include ActiveHash::Associations
--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -1,16 +1,16 @@
 class ProductCategory < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
-    { id: 2, name: 'レディース' },
-    { id: 3, name: 'メンズ' },
-    { id: 4, name: 'ベビー・キッズ' },
-    { id: 5, name: 'インテリア・住まい・小物' },
-    { id: 6, name: '本・音楽・ゲーム' },
-    { id: 7, name: 'おもちゃ・ホビー・グッズ' },
-    { id: 8, name: '家電・スマホ・カメラ' },
-    { id: 9, name: 'スポーツ・レジャー' },
-    { id: 10, name: 'ハンドメイド' },
-    { id: 11, name: 'その他' }
+    { id: 2, name: '一般 前期' },
+    { id: 3, name: '共通テスト利用 前期' },
+    { id: 4, name: '共通テスト利用 中期' },
+    { id: 5, name: '共通テスト利用 後期' },
+    { id: 6, name: '一般 後期' },
+    { id: 7, name: 'その他' },
+    # { id: 8, name: '家電・スマホ・カメラ' },
+    # { id: 9, name: 'スポーツ・レジャー' },
+    # { id: 10, name: 'ハンドメイド' },
+    # { id: 11, name: 'その他' }
   ]
 
   include ActiveHash::Associations
