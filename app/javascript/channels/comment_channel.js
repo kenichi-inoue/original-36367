bd684ead57da47124f26619d0be9c4715ee31eef
kenichi-inoue <ken1.inoue13@gmail.com>
2022-01-03 00:39:02 +0900

    ここまでで記入したらすぐ動く状態

--- app/javascript/channels/comment_channel.js
+++ app/javascript/channels/comment_channel.js
@@ -1,5 +1,8 @@
 import consumer from "./consumer"
 
+if(location.pathname.match(/\/products\/\d/)){
+  // console.log("読み込み完了")
+
 consumer.subscriptions.create("CommentChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
@@ -11,6 +14,7 @@ consumer.subscriptions.create("CommentChannel", {
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
+    // console.log(data) //追加
     const html = `
     <div class="comment">
       <p class="user-info">${data.user.nickname}： </p>
@@ -20,5 +24,8 @@ consumer.subscriptions.create("CommentChannel", {
   comments.insertAdjacentHTML('beforeend', html)
   const commentForm = document.getElementById("comment-form") 
   commentForm.reset();
+
   }
 });
+
+}
